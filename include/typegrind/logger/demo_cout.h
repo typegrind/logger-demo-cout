4b3076f807cf1e540a80118beaad43be344a6eb0
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-03-28 09:45:17 -0700

    initial commit (reworked version with the new api)

---
+++ include/typegrind/logger/demo_cout.h
@@ -0,0 +1,154 @@
+
+#ifndef TYPEGRIND_LOGGER_NOP_H_
+#define TYPEGRIND_LOGGER_NOP_H_
+
+#define TYPEGRIND_LOG_NEW(typeStr, locationStr, typeSize, newExpression) ( typegrind::logger::entry_alloc<typegrind::logger::demo_cout>{typeStr, locationStr, typeSize, 0} * (newExpression ) )
+#define TYPEGRIND_LOG_NEW_ARRAY(typeStr, locationStr, typeSize, size, newExpression) ( typegrind::logger::entry_alloc<typegrind::logger::demo_cout>{typeStr, locationStr, typeSize, size} * (newExpression ) )
+#define TYPEGRIND_LOG_OP_NEW(typeStr, locationStr, typeSize, newExpression) ( typegrind::logger::entry_alloc<typegrind::logger::demo_cout>{typeStr, locationStr, typeSize, 0} * (newExpression ) )
+#define TYPEGRIND_LOG_OP_NEW_ARRAY(typeStr, locationStr, typeSize, size, newExpression) ( typegrind::logger::entry_alloc<typegrind::logger::demo_cout>{typeStr, locationStr, typeSize, size} * (newExpression ) )
+
+#define TYPEGRIND_LOG_DELETE(pointerAddr, locationStr, deleteExpression) typegrind::logger::entry_free<typegrind::logger::demo_cout>(pointerAddr, locationStr) ; ( deleteExpression )
+#define TYPEGRIND_LOG_DELETE_ARRAY(pointerAddr, locationStr, deleteExpression) typegrind::logger::entry_free<typegrind::logger::demo_cout>(pointerAddr, locationStr) ; ( deleteExpression )
+#define TYPEGRIND_LOG_OP_DELETE(pointehAddr, locationStr, deleteExpression) typegrind::logger::entry_free<typegrind::logger::demo_cout>(pointerAddr, locationStr) ; ( deleteExpression ) 
+#define TYPEGRIND_LOG_OP_DELETE_ARRAY(pointerAddr, locationStr, deleteExpression) typegrind::logger::entry_free<typegrind::logger::demo_cout>(pointerAddr, locationStr) ; ( deleteExpression ) 
+
+#define TYPEGRIND_LOG_METHOD_INITIALIZER(targetName, locationStr, customName, flags, initializerExpression) ( typegrind::logger::initializer_scope<typegrind::logger::demo_cout>(targetName, locationStr, customName, flags) ? (initializerExpression) : (initializerExpression) )
+#define TYPEGRIND_LOG_METHOD_ENTER(targetName, locationStr, customName, flags) typegrind::logger::method_scope<typegrind::logger::demo_cout> typegrind_scope_guard(targetName, locationStr, customName, flags);
+
+#define TYPEGRIND_DEMANGLE(s) abi::__cxa_demangle((s), typegrind::logger::demangle_buffer, NULL, &typegrind::logger::demangle_status_code) 
+
+namespace typegrind
+{
+  namespace logger
+  {
+    template<typename T>
+    struct entry_alloc
+    {
+      const char*         typeStr;
+      const char*         locationStr;
+      const unsigned int  typeSize;
+      const unsigned int  size;
+      const void* ptr;
+    };
+
+    template<typename T, typename O>
+    O* operator*(entry_alloc<T> alloc_info, O* ptr)
+    {
+      alloc_info.ptr = ptr;
+      T::get().log_alloc(alloc_info);
+      return ptr;
+    }
+
+    template<typename T, typename O>
+    const O* operator*(entry_alloc<T> alloc_info, const O* ptr)
+    {
+      alloc_info.ptr = ptr;
+      T::get().log_alloc(alloc_info);
+      return ptr;
+    }
+
+    // -----------------------------------------------
+
+    template<typename T>
+    struct entry_free
+    {
+      const void* const   ptr;
+      const char*         locationStr;
+
+      entry_free(const void* const ptr, const char* locationStr)
+      : ptr(ptr)
+      , locationStr(locationStr)
+      {
+        T::get().log_free(*this);
+      }
+    };
+
+    // -----------------------------------------------
+
+    template<typename T>
+    struct method_scope
+    {
+      typedef unsigned long flags_type;
+
+      const char*         targetName;
+      const char*         locationStr;
+      const char*         customName;
+      flags_type          flags;
+
+      method_scope(const char* targetName, const char* locationStr, const char* customName, flags_type flags)
+      : targetName(targetName)
+      , locationStr(locationStr)
+      , customName(customName)
+      , flags(flags)
+      {
+        T::get().log_method_enter(*this);
+      }
+
+      ~method_scope()
+      {
+        T::get().log_method_exit(*this);
+      }
+    };
+
+    // -----------------------------------------------
+
+    template<typename T>
+    struct initializer_scope
+    {
+      typedef unsigned long flags_type;
+
+      const char*         targetName;
+      const char*         locationStr;
+      const char*         customName;
+      flags_type          flags;
+
+      initializer_scope(const char* targetName, const char* locationStr, const char* customName, flags_type flags)
+      : targetName(targetName)
+      , locationStr(locationStr)
+      , customName(customName)
+      , flags(flags)
+      {
+        T::get().log_initializer_enter(*this);
+      }
+
+      ~initializer_scope()
+      {
+        T::get().log_initializer_exit(*this);
+      }
+
+      explicit operator bool() const
+      {
+        return true;
+      }
+    };
+
+    static int demangle_status_code;
+    static char* demangle_buffer;
+
+
+
+
+
+    class demo_cout
+    {
+    public:
+      demo_cout();
+      ~demo_cout();
+      
+      void log_alloc(entry_alloc<demo_cout> const& alloc_info);
+      void log_free(entry_free<demo_cout> const& free_info);
+
+      void log_method_enter(method_scope<demo_cout> const& alloc_info);
+      void log_method_exit(method_scope<demo_cout> const& free_info);
+
+      void log_initializer_enter(initializer_scope<demo_cout> const& method_info);
+      void log_initializer_exit(initializer_scope<demo_cout> const& initializer_info);
+
+      static demo_cout& get();
+    };
+
+
+  }
+}
+
+#endif
---
+++ src/demo_cout.cpp
@@ -0,0 +1,63 @@
+
+#include <typegrind/logger/demo_cout.h>
+
+#include <iostream>
+#include <bitset>
+
+namespace typegrind
+{
+  namespace logger
+  {
+    demo_cout::demo_cout()
+    {
+      std::cout << "logger started" << std::endl;
+    }
+
+    demo_cout::~demo_cout()
+    {
+      std::cout << "logger terminating" << std::endl;
+    }
+
+    void demo_cout::log_alloc(entry_alloc<demo_cout> const& alloc_info)
+    {
+      std::cout << "allocation at:" << alloc_info.locationStr << " type:" << alloc_info.typeStr << " type_size:" << alloc_info.typeSize << " total_size:" << alloc_info.size << " addr:" << alloc_info.ptr << std::endl;
+    }
+
+    void demo_cout::log_free(entry_free<demo_cout> const& free_info)
+    {
+      std::cout << "free at:" << free_info.locationStr << " addr:" << free_info.ptr << std::endl;
+    }
+
+    void demo_cout::log_method_enter(method_scope<demo_cout> const& method_info)
+    {
+      std::cout << "method enter at:" << method_info.locationStr << " method:" << method_info.targetName << " custom_name:" << method_info.customName << " flags:" << std::bitset<32>(method_info.flags) << std::endl;
+    }
+
+    void demo_cout::log_method_exit(method_scope<demo_cout> const& method_info)
+    {
+      std::cout << "method exit at:" << method_info.locationStr << " method:" << method_info.targetName << " custom_name:" << method_info.customName << " flags:" << std::bitset<32>(method_info.flags) << std::endl;
+    }
+
+    void demo_cout::log_initializer_enter(initializer_scope<demo_cout> const& initializer_info)
+    {
+      std::cout << "initializer enter at:" << initializer_info.locationStr << " method:" << initializer_info.targetName << " custom_name:" << initializer_info.customName << " flags:" << std::bitset<32>(initializer_info.flags) << std::endl;
+    }
+
+    void demo_cout::log_initializer_exit(initializer_scope<demo_cout> const& initializer_info)
+    {
+      std::cout << "initializer exit at:" << initializer_info.locationStr << " method:" << initializer_info.targetName << " custom_name:" << initializer_info.customName << " flags:" << std::bitset<32>(initializer_info.flags) << std::endl;
+    }
+
+    demo_cout& demo_cout::get()
+    {
+      static demo_cout instance;
+
+      return instance;
+    }
+  }
+
+
+  static int demangle_status_code = 0;
+  static char* demangle_buffer = (char*)malloc(2000);
+}
+
